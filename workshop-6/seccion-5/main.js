43eb385910e28e209d1af5f3a575e5b2ad28c073
Mahi_G13 <maicolgallego14@gmail.com>
2024-04-29 21:24:31 -0500

    Add secund then

--- workshop-6/seccion-5/main.js
+++ workshop-6/seccion-5/main.js
@@ -1,30 +1,69 @@
-
 const getData = () => {
-    return new Promise((resolve,reject) => {
-        setTimeout(()=>{
-            fetch('data.json').
-         then(response => response.json()).
-         then(data => {
-            console.log('Habitaciones', data.rooms);
-            console.log('Tipo de habitaciones', data.roomTypes)
-            resolve(data) 
-         }).
-         catch((error) => {
-            console.error(error)
-            reject(error) 
-         });
-        },2000)
-    });
-}
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      fetch("data.json")
+        .then((response) => response.json())
+        .then((data) => {
+          console.log("Habitaciones", data.rooms);
+          console.log("Tipo de habitaciones", data.roomTypes);
+          resolve(data);
+        })
+        .catch((error) => {
+          console.error(error);
+          reject(error);
+        });
+    }, 2000);
+  });
+};
 
 getData()
-.then(({rooms, roomTypes}) => {
-    const userInput = prompt('Ingrese cuantas personas se van a alojar?');
-    for (bed of roomTypes){
-        if(bed.capacity >= parseInt(userInput)){
-            console.log(`Las habitaciones que cumplen con ese criterio son ${bed.name}`)
-        }
+    .then(({ rooms, roomTypes }) => {
+    const userInput = prompt("Ingrese cuantas personas se van a alojar?");
+    let room = `Las habitaciones que son para ${userInput}`;
+    for (bed of roomTypes) {
+      if (bed.capacity >= parseInt(userInput)) {
+        room += `, ${bed.name}`;
+      }
     }
-    
-    
-})
+    console.log(room);
+    let selectedRoom = prompt(
+      "Ingrese el nombre de la habitacion que desea reservar"
+    );
+        
+    return  [roomTypes, selectedRoom];
+    })
+    .then(([roomTypes, selectedRoom]) => {
+      console.log(selectedRoom);
+          let findRoom = 0;
+        for (let i = 0; i < roomTypes.length; i++) {
+            if (roomTypes[i].name === selectedRoom) {
+              findRoom = roomTypes[i].id;
+              console.log(findRoom);
+              break;
+            }
+          }
+    })
+  
+
+
+
+
+
+
+
+
+
+
+
+//   .then(([selectedRoom, roomTypes]) => {
+//     
+//     return findRoom;
+//   })
+//   .then((findRoom) => {
+//     for (roomId in roomTypes) {
+//       if (roomId.id === findRoom) {
+//         console.log(roomId.name, roomId.priceNight);
+//         break;
+//       }
+//     }
+//   });
